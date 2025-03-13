d835e9828d9911802f16ff8d73a3fe9ebbed4f08
Pratham Jain <jainprathamj24@gmail.com>
2025-03-13 16:30:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Task 4 deployed\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
